819cd361634d51834c3af5b255cf3942df4acbc3
Jim Wu <31392245+jimmywutangclan@users.noreply.github.com>
2026-03-26 17:33:44 -0400

    add door collider pause when interacting

--- Scripts/Objects/Door.gd
+++ Scripts/Objects/Door.gd
@@ -4,6 +4,7 @@ extends Node
 @export var Audio_Player: AudioStreamPlayer3D
 @export var Audio: AudioStreamMP3
 @export var Start_Open: bool = false
+@export var Collision_Shape: CollisionShape3D
 
 var Action_Locked: bool
 var Is_Open: bool
@@ -14,6 +15,8 @@ func _ready() -> void:
 		Animation_Player.play("OpenDoor")
 	Action_Locked = false
 	Is_Open = Start_Open
+	if Collision_Shape == null:
+		Collision_Shape = get_node("CollisionShape3D")
 
 func Interact():
 	if Is_Open == true:
@@ -28,6 +31,8 @@ func Open():
 		Audio_Player.play()
 		Action_Locked == true
 		Is_Open = true
+		process_mode = Node.PROCESS_MODE_PAUSABLE
+		Collision_Shape.disabled = true
 		
 func Close():
 	if Action_Locked == false:
@@ -36,6 +41,8 @@ func Close():
 		Audio_Player.play()
 		Action_Locked == true
 		Is_Open = false
+		Collision_Shape.disabled = true
 
 func _on_animation_player_current_animation_changed(name: StringName) -> void:
 	Action_Locked == false
+	Collision_Shape.disabled = false
